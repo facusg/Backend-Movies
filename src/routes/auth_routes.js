de4435dd92fbdad2bb1c5c6dd5e790a074008736
Facundo <facusgalante@gmail.com>
2021-07-07 22:55:51 -0300

    Login y logout terminado

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -2,22 +2,57 @@ const express = require("express");
 const conexion = require("../conexions");
 const router = express.Router();
 
+router.get("/check", (req, res) => {
+  if (req.session.user) {
+    res.json({ message: "ok", data: req.session.user });
+  } else {
+    res.json({ message: "error" });
+  }
+});
+
 router.post("/", (req, res) => {
   email = req.body.email;
   password = req.body.password;
 
-  const sql = `SELECT name,email FROM users WHERE email=? AND password=? `;
+  const sql = `SELECT * FROM users WHERE email=? AND password=? `;
 
   conexion.query(sql, [email, password], (err, result) => {
     if (err) {
       console.log("Error en la conexion auth");
     } else {
       if (result.length === 1) {
-        req.session.user = { name: result[0].name };
+        req.session.user = {
+          user: {
+            id: result[0].id,
+            name: result[0].name,
+            email: result[0].email,
+          },
+        };
+
         console.log(req.session.user);
+
         console.log(result);
-        res.json({ message: "Usuario valido" });
-      } else res.json({ auth: false, message: "Usuario incorrecto" });
+
+        res.status(200).json({
+          message: "Usuario valido",
+          user: {
+            id: result[0].id,
+            name: result[0].name,
+            email: result[0].email,
+          },
+        });
+      } else
+        res.status(401).json({ message: "Email y/o contraseña incorrecta" });
+    }
+  });
+});
+
+router.delete("/", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(500).json({ message: "Erro en el cierre de sesion" });
+    } else {
+      res.status(200).json({ message: "Sesion Cerrada" });
     }
   });
 });
--- src/routes/favorites_routes.js
+++ src/routes/favorites_routes.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const idfavorite = req.params.id;
-  const sql = "SELECT * FROM favorites Where id=?";
+  const sql = "SELECT * FROM favorites Where id_favorite=?";
   conexion.query(sql, [idfavorite], (err, result) => {
     if (err) {
       res.send("Error al obtener los favoritos");
@@ -26,16 +26,38 @@ router.get("/:id", (req, res) => {
   });
 });
 
-router.post("/:id", (req, res) => {
-  const favorite = req.body.favorite;
+router.post("/", (req, res) => {
+  const id = req.body.id;
+  const path = req.body.path;
+  const title = req.body.title;
+  const overview = req.body.overview;
+  const vote = req.body.vote;
 
-  const sql = `INSERT INTO favorites (favorite) 
-                  VALUES (?)`;
-  conexion.query(sql, [favorite], (err, result) => {
+  const sql1 = "SELECT * FROM favorites WHERE id_favorite=?";
+  conexion.query(sql1, [id], (err, result) => {
     if (err) {
       res.send("Error al insertar un favorito");
     } else {
-      res.send("Favorito agregado");
+      if (result.length === 1) {
+        console.log("Este ya lo agrege");
+      } else {
+        const sql2 = `INSERT INTO favorites 
+              (id_favorite,backdrop_path,title,overview,vote_average) 
+                  VALUES (?,?,?,?,?)`;
+        conexion.query(
+          sql2,
+          [id, path, title, overview, vote],
+          (err, result) => {
+            if (err) {
+              res.send("Error al insertar un favorito");
+            } else {
+              console.log(result);
+              console.log("favorito agregado");
+              res.send({ message: "Favorito Agregado", id });
+            }
+          }
+        );
+      }
     }
   });
 });
