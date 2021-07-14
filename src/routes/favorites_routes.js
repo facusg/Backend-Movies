9c0742cd4cc6b55a19b314b9d1cebd8575165347
Facundo <facusgalante@gmail.com>
2021-07-13 21:51:52 -0300

    Favoritos terminado

--- src/routes/favorites_routes.js
+++ src/routes/favorites_routes.js
@@ -14,46 +14,100 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/:id", (req, res) => {
-  const idfavorite = req.params.id;
-  const sql = "SELECT * FROM favorites Where id_favorite=?";
-  conexion.query(sql, [idfavorite], (err, result) => {
+router.get("/:id/:idMovie", (req, res) => {
+  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  console.log(req.params.id);
+  console.log(req.params.idMovie);
+  const idUser = req.params.id;
+  const idMovie = req.params.idMovie;
+  const sql = "SELECT * FROM usersfavorites Where idser=? AND idfavorite=?";
+  conexion.query(sql, [idUser, idMovie], (err, result) => {
     if (err) {
-      res.send("Error al obtener los favoritos");
+      res.status(400).json({ message: "Error al verificar favorito" });
     } else {
-      res.json(result);
+      if (result.length === 1) {
+        res.status(200).send({ message: "Favorito", active: true });
+      } else {
+        res.status(401).send({ message: "NoFavorito", active: false });
+      }
     }
   });
 });
 
 router.post("/", (req, res) => {
-  const id = req.body.id;
-  const path = req.body.path;
-  const title = req.body.title;
+  const idMovie = req.body.idMovie;
+  const idUser = req.body.idUser;
+  const path = req.body.backdrop_path;
+  const title = req.body.name;
   const overview = req.body.overview;
-  const vote = req.body.vote;
+  const vote = req.body.vote_average;
+  const trailerUrl = req.body.trailerUrl;
+  console.log(req.body);
+  const sql1 = "SELECT * FROM moviesyseries WHERE idAPI=?";
+  const sql2 = `INSERT INTO moviesyseries 
+                (idAPI,backdrop_path,title,overview,vote_average,trailerUrl) 
+                VALUES (?,?,?,?,?,?)`;
+  const slq3 = `INSERT INTO usersfavorites (idser,idfavorite) VALUES (?,?)`;
+  const sql4 = "SELECT * FROM usersfavorites WHERE idser=? AND idfavorite=?";
 
-  const sql1 = "SELECT * FROM favorites WHERE id_favorite=?";
-  conexion.query(sql1, [id], (err, result) => {
+  conexion.query(sql1, [idMovie], (err, result1) => {
     if (err) {
       res.send("Error al insertar un favorito");
     } else {
-      if (result.length === 1) {
-        console.log("Este ya lo agrege");
+      if (result1.length === 1) {
+        conexion.query(sql4, [idUser, idMovie], (err, result2) => {
+          if (err) {
+            res
+              .status(400)
+              .json({ message: "Error al buscar en tabla users-favorites" });
+          } else {
+            console.log("---------------------");
+            console.log(result2);
+            console.log(result2.length);
+
+            if (result2.length === 1) {
+              res
+                .status(200)
+                .json({ message: "Este ya esta en tus favoritos" });
+              console.log("lo agrege ");
+            } else {
+              conexion.query(slq3, [idUser, idMovie], (err, result) => {
+                if (err) {
+                  console.error(req.body);
+                  res.status(400).json({
+                    message: "Error al insertar en tabla users-favorites",
+                  });
+                } else {
+                  res
+                    .status(200)
+                    .json({ message: "Favorito guardado correctamente" });
+                }
+              });
+            }
+          }
+        });
       } else {
-        const sql2 = `INSERT INTO favorites 
-              (id_favorite,backdrop_path,title,overview,vote_average) 
-                  VALUES (?,?,?,?,?)`;
         conexion.query(
           sql2,
-          [id, path, title, overview, vote],
+          [idMovie, path, title, overview, vote, trailerUrl],
           (err, result) => {
             if (err) {
-              res.send("Error al insertar un favorito");
+              console.error(err);
+              res.status(400).json({
+                message: "1 Error al insertar en tabla moviesyseries",
+              });
             } else {
-              console.log(result);
-              console.log("favorito agregado");
-              res.send({ message: "Favorito Agregado", id });
+              conexion.query(slq3, [idUser, idMovie], (err, result) => {
+                if (err) {
+                  res.status(400).json({
+                    message: "Error al insertar en tabla users-favorites",
+                  });
+                } else {
+                  res
+                    .status(200)
+                    .json({ message: "Favorito guardado correctamente" });
+                }
+              });
             }
           }
         );
